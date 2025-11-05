81bc53fcd9e354f6c3168cbe52a318e9ba59db93
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-11-05 22:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	for (let i=0;i<str. length;i++) {
+		if(str. includes(str[i]) && str[i+1]!= str[i]){
+			return i;
+		}else{
+			return null;
+		}
+	}
+	
 }
-const input = prompt("Enter a string");
+
+const input = prompt("aabbcdd");
 alert(firstNonRepeatedChar(input)); 
+console. log(firstNonRepeatedChar(input));
