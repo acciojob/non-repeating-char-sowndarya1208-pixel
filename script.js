c7ab16a9e59d003ffc2acc29b81a6bbb1e74123e
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-11-05 23:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-
-	for (let i=0;i<str. length;i++) {
-		if(str. includes(str[i]) && str[i+1]!= str[i]){
-			return i;
-		}else{
-			return null;
-		}
-	}
-	
+const freq = {};
+    for (const char of str) {
+        freq[char] = (freq[char] || 0) + 1;
+    }
+    for (const char of str) {
+        if (freq[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
+
+	
+
+			
+		
+	}
+
 
 const input = prompt("aabbcdd");
 alert(firstNonRepeatedChar(input)); 
